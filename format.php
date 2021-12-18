4b9e726c883bee5ea1a4101d7840e934489eb785
Saurabh Shankar Khade <66903901+SaurabhKhade@users.noreply.github.com>
2021-12-18 20:26:57 +0530

    format file added

---
+++ format.php
@@ -0,0 +1,19 @@
+<?php 
+  include './templates/start_document.php';
+  include './templates/end_document.php';
+
+  start_document("Title");
+?>
+
+//write your code from here, start code directly which is to be added in body. All other tags will be added automatically
+//html
+//head
+//body
+
+
+//body
+//html
+
+<?php 
+  end_document();
+?>

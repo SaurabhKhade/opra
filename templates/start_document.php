8c20d39a76ffe3126e3701f25d432ee1dcfc4a32
Saurabh Shankar Khade <66903901+SaurabhKhade@users.noreply.github.com>
2021-12-18 20:27:59 +0530

    Update start_document.php

--- templates/start_document.php
+++ templates/start_document.php
@@ -1,14 +1,17 @@
 <?php
   function start_document($title) {
     $start = <<<"EOT"
-      <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="/static/bootstrap/bootstrap.min.css">
-        <link rel="stylesheet" href="/static/font_awesome/css/font-awesome.min.css">
-        <title>$title</title>
-      </head>
+    <!DOCTYPE html>
+      <html lang="en">  
+        <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <link rel="stylesheet" href="/static/bootstrap/bootstrap.min.css">
+          <link rel="stylesheet" href="/static/font_awesome/css/font-awesome.min.css">
+          <title>$title</title>
+        </head>
+      <body>
     EOT;
 
     echo $start;
